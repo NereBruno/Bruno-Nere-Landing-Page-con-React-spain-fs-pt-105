abf3a6fdda9ce38672b0117f49857b1fd8b5299b
Bruno da Silva Nere <bsnbsb@gmail.com>
2025-05-11 19:53:26 +0000

    soluciones

--- src/js/components/Jumbotron/Jumbotron.jsx
+++ src/js/components/Jumbotron/Jumbotron.jsx
@@ -4,7 +4,7 @@ const Jumbotron = () => {
     return (
         <div className="jumbotron bg-success bg-gradient py-5 mb-4 text-white">
             <div className="container text-center">
-                <h1 className="display-5 fw-bold">¡Aventuras Inolvidables!</h1>
+                <h1 className="display-5 fw-bold">¡Vacaciones por Europa!</h1>
                 <p className="lead opacity-75">Descubre los mejores destinos con guías locales</p>
                 <button className="btn btn-light btn-lg mt-3 px-4 fw-semibold">
                     Ver Catálogo
